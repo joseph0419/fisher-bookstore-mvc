a8dbdb3b76bf8224c24794e8b30d64d9cd52151d
Xiuyuan Da <yuxiuyuan0324@gmail.com>
2019-02-06 18:54:32 -0500

    compeledLab3

---
+++ Fisher.Bookstore/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers
+{
+    [Route("books")]
+public class BooksController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index() {
+            return View();
+        }
+        [Route("new")]
+        public IActionResult New() {
+            return View();
+        }
+
+        [Route("best-sellers")]
+        public IActionResult Best() {
+            return View();
+        }
+
+      
+    }
+}
